b78af03cd2b8aecd35471abc34e690fd9f162b97
AndrewFC <andrzej.chesiak@gmail.com>
2015-05-24 16:05:30 +0200

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,43 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This are functions written for Programming Assignment 2, 
+## from 'R Programming' (May 4, 2015 - July 1, 2015).
+#
+# Together, they can be used to avoid computational cost of inversing the same matrix
+# multiple times.
+#
+#
+## Function makeCacheMatrix
+# 
+# This function takes matrix 'x' creatres special 'matrix-like' object, that stores both 
+# the matrix and its inversion (technically it creates a list of four element).
 
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
+    m <- NULL                              # Cache initially set empty.
+    set <- function(y) {                   # A 'subfunction' for creating a matrix.
+        m <<- NULL                         #
     }
-    get <- function() {
-        x
+    get <- function() {                    # A 'subfunction' for seting values 
+        x                                  # inside the matrix.
     }
-    setinvert <- function(inverse) {
-        m <<- inverse
+    setinvert <- function(inverse) {       # A 'subfunction' for inverting the matrix
+        m <<- inverse                      # and storing it in the cache.
     }
-    getinvert <- function() {
-        m
+    getinvert <- function() {              # A 'subfunction' calls inverted matrix 
+        m                                  # from the cache.
     }
-    list(set = set, 
+    list(set = set,                        # List of all four 'sub-functions'.
          get = get,
          setinvert = setinvert,
          getinvert = getinvert)
 }
 
 
-## Write a short comment describing this function
+## Function cacheSolve
+#
+# Takes as input the output of function makeCacheMatrix and prints inversion of matrix 'x'
 
 cacheSolve <- function(x , ...) {
-    m <- x$getinvert()
-    if(!is.null(m)) {
-        message("getting cached data")
-        return(m)
+    m <- x$getinvert()                      # Calls inverted matrix from the cache
+    if(!is.null(m)) {                       # If cache is non empty...
+        message("getting cached data")      # 
+        return(m)                           # ...inverted matrix is taken from cache
     }
-    data <- x$get()
+    data <- x$get()                         # If else, inverted matrix is calculated
     m <- solve(data, ...)
     x$setinvert(m)
     m
 }
+
+## Example 
 
 samplematrix <- matrix(c(1,0,4, 1,3,4, 4,1,0), nrow = 3, ncol = 3, byrow = TRUE,
                         dimnames = list(c("row1", "row2", "row3"),
